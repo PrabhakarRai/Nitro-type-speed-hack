a0fe746c01e25677e3962c5da2421e40aa02412a
Prabhakar Rai <student343.bhu@gmail.com>
2020-05-04 22:40:14 +0530

    Create race.user.js

---
+++ race.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Nitro Typer 5.0
+// @namespace    https://www.theprabhakar.in/
+// @version      5.0
+// @description  Browser counterpart of Nitro Typer 5.0
+// @author       Prabhakar Rai
+// @match        https://www.nitrotype.com/race/*
+// @match        https://www.nitrotype.com/race
+// @grant GM_getValue
+// @grant GM_listValues
+// @grant GM_getResourceText
+// @grant GM_getResourceURL
+// @grant GM_setClipboard
+// @grant GM_info
+// @grant GM_getMetadata
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    //https://code.jquery.com/jquery-2.1.4.min.js
+    //under construction
+
+})();
